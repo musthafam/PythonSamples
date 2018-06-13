5a6163fa1f11c897e6d097f9c5d72c3c9588b493
musthafam <mmmgce@gmail.com>
2018-06-13 21:18:59 +0530

    initialCommit

---
+++ sampleDataRetrievalFromExcel.py
@@ -0,0 +1,24 @@
+# Sample script to retrieve value from excel sheet
+# Using xlrd library
+
+import xlrd
+
+# Get the number of Rows in a sheet
+def getNoOfRows(sheetname):
+	return sheetname.nrows
+
+# Get the number of Columns in a sheet	
+def getNoOfColumns(sheetname):
+	return sheetname.ncols
+
+# Get the values from a cell
+def getCellValuesFromXL(sheetname,rn,cn):
+	return sheetname.cell_value(rn,cn)		
+
+
+book = xlrd.open_workbook('Sample.xlsx')
+sheet = book.sheet_by_index(0)
+print(getCellValuesFromXL(sheet,0,0))
+print(getCellValuesFromXL(sheet,0,1))
+print(getNoOfRows(sheet))
+print(getNoOfColumns(sheet))
